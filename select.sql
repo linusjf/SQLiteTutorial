809f011a1b458c3ebd55a8757d499577fc954fd8
Linus Fernandes <linus.fernandes@gmail.com>
2024-12-06 15:32:10 +0530

    feat: start select order by command

--- select.sql
+++ select.sql
@@ -41,3 +41,76 @@ If someone adds more columns, your application may work but it gets more data th
 So try to avoid using the asterisk (*) as a good habit when you use the SELECT statement.
 */
 select * from albums;
+/*
+To sort the rows in a result set, you add the ORDER BY clause to the  SELECT statement as follows:
+
+SELECT
+   select_list
+FROM
+   table
+ORDER BY
+    column_1 ASC,
+    column_2 DESC;
+Code language: SQL (Structured Query Language) (sql)
+The ORDER BY clause comes after the FROM clause. It allows you to sort the result set based on one or more columns in ascending or descending order.
+
+In this syntax, you place the column name by which you want to sort after the ORDER BY clause followed by the ASC or DESC keyword.
+
+The ASC keyword means ascending.
+The DESC keyword means descending.
+If you don’t specify the ASC or DESC keyword, SQLite sorts the result set using the ASC option. In other words, it sorts the result set in ascending order by default.
+
+If you want to sort the result set by multiple columns, you use a comma (,) to separate two columns.
+
+The ORDER BY clause sorts rows using columns or expressions from left to right. In other words, the ORDER BY clause sorts the rows using the first column in the list. Then, it sorts the sorted rows using the second column, and so on.
+
+SQLite allows you to sort the result set using columns that do not appear in the select list of the SELECT clause.
+*/
+/*
+Suppose you want to sort the result set based on AlbumId column in ascending order, you use the following statement:
+*/
+SELECT
+  name,
+  milliseconds,
+  albumid
+FROM
+  tracks
+ORDER BY
+  albumid ASC;
+/*
+SQLite uses ASC by default so you can omit it in the above statement as follows:
+*/
+SELECT
+  name,
+  milliseconds,
+  albumid
+FROM
+  tracks
+ORDER BY
+  albumid;
+/*
+Suppose you want to sort the sorted result (by AlbumId) above by the Milliseconds column in descending order. In this case, you need to add the Milliseconds column to the ORDER BY clause as follows:
+*/
+SELECT
+  name,
+  milliseconds,
+  albumid
+FROM
+  tracks
+ORDER BY
+  albumid ASC,
+  milliseconds DESC;
+/*
+SQLite sorts rows by AlbumId column in ascending order first. Then, it sorts the sorted result set by the Milliseconds column in descending order.
+
+If you look at the tracks of the album with AlbumId 1, you’ll find that the order of tracks changes between the two statements.*/
+SELECT
+  name,
+  milliseconds,
+  albumid
+FROM
+  tracks
+ORDER BY
+  albumid ASC,
+  milliseconds DESC
+LIMIT 5;
