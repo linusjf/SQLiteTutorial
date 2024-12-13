0a649d98ffaaccad5c3b890b6d2db595835e39c2
Linus Fernandes <linus.fernandes@gmail.com>
2024-12-13 21:48:56 +0530

    feat: add EXCEPT examples to select

--- select.sql
+++ select.sql
@@ -2150,3 +2150,94 @@ FROM
 ORDER BY
   FirstName,
   LastName;
+
+/*
+SQLite EXCEPT operator compares the result sets of two queries and returns distinct rows from the first query that are not output by the second query.
+
+The following shows the syntax of the EXCEPT operator:
+
+SELECT select_list1
+FROM table1
+EXCEPT
+SELECT select_list2
+FROM table2;
+This query must conform to the following rules:
+
+First, the number of columns in the select lists of both queries must be the same.
+Second, the order of the columns and their types must be comparable.
+The following statements create two tables t1 and t2 and insert some data into both tables:
+
+The following statement illustrates how to use the EXCEPT operator to compare result sets of two queries:
+*/
+SELECT c1 FROM t1
+EXCEPT 
+SELECT c2 FROM t2;
+
+/*
+First, create new tables books, customers, and orders:
+*/
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS custs;
+DROP TABLE IF EXISTS books;
+CREATE TABLE books (
+    book_id INTEGER PRIMARY KEY,
+    title TEXT,
+    author TEXT,
+    genre TEXT,
+    price REAL
+);
+
+CREATE TABLE custs (
+    customer_id INTEGER PRIMARY KEY,
+    name TEXT,
+    email TEXT
+);
+
+CREATE TABLE orders (
+    order_id INTEGER PRIMARY KEY,
+    customer_id INTEGER,
+    book_id INTEGER,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES custs(customer_id),
+    FOREIGN KEY (book_id) REFERENCES books(book_id)
+);
+/*
+Second, insert rows into these tables:
+*/
+INSERT INTO books (title, author, genre, price) 
+VALUES
+('To Kill a Mockingbird', 'Harper Lee', 'Fiction', 12.99),
+('1984', 'George Orwell', 'Science Fiction', 10.99),
+('The Great Gatsby', 'F. Scott Fitzgerald', 'Classic', 9.99),
+('Pride and Prejudice', 'Jane Austen', 'Romance', 8.99),
+('The Catcher in the Rye', 'J.D. Salinger', 'Fiction', 11.99);
+
+INSERT INTO custs (name, email) 
+VALUES
+('Alice', 'alice@gmail.com'),
+('Bob', 'bob@yahoo.com'),
+('Charlie', 'charlie@outlook.com');
+
+INSERT INTO orders (customer_id, book_id, order_date) 
+VALUES
+(1, 1, '2024-04-25'), -- Alice purchased 'To Kill a Mockingbird'
+(1, 3, '2024-04-27'), -- Alice purchased 'The Great Gatsby'
+(2, 2, '2024-04-26'); -- Bob purchased '1984'
+
+/*
+Third, use the EXCEPT operator to find books that have not had any sales:
+*/
+SELECT title, author, genre, price
+FROM books
+EXCEPT
+SELECT title, author, genre, price
+FROM books
+JOIN orders using(book_id);
+
+/*
+How it works.
+
+The first query selects all books from the books table.
+The second query retrieves books that have orders by joining the books table with the orders table.
+The EXCEPT operator returns a list of distinct books that have not been ordered by any customer.
+*/
