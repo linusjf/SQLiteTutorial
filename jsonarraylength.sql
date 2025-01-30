430d4f5098ea079a2a1268e8544f02a23a9713eb
Linus Fernandes <linus.fernandes@gmail.com>
2025-01-30 14:09:39 +0530

    feat: add json_array_length sql

--- jsonarraylength.sql
+++ jsonarraylength.sql
@@ -36,6 +36,58 @@ SELECT JSON_ARRAY_LENGTH('{"reviews": [1,2,3,4,5]}', '$.reviews');
 The following example uses the json_array_length() function to return the number of elements in a non-JSON array:
 */
 SELECT JSON_ARRAY_LENGTH('{"reviews": [1,2,3,4,5]}', '$.reviews[1]') AS result;
+
 /*
 It returns zero because the path '$.reviews[1]' returns the first element of the reviews array, which is the number 1.
 */
+/*
+4) Getting the number of elements of a path that does not exist
+The following example uses the json_array_length() function to return the number of elements in an array specified by a path that does not exist:
+*/
+SELECT JSON_ARRAY_LENGTH('{"name": "Alice"}', '$.age') AS result;
+
+/*
+It returns NULL because the path '$.age' does not select any elements in the JSON data.
+*/
+/*
+5) Using the json_array_length() function with table data
+First, create a table called animals:
+*/
+DROP TABLE IF EXISTS animals;
+
+CREATE TABLE animals (
+  id INTEGER PRIMARY KEY,
+  data TEXT NOT NULL
+);
+
+/*
+Second, insert rows into the animals table:
+*/
+INSERT INTO animals
+  (data)
+VALUES
+  ('{"animal": "dog", "colors": ["brown", "white", "black"]}');
+
+INSERT INTO animals
+  (data)
+VALUES
+  ('{"animal": "cat", "colors": ["gray", "orange", "black"]}');
+
+INSERT INTO animals
+  (data)
+VALUES
+  ('{"animal": "chicken", "colors": ["white", "brown"]}');
+
+/*
+Third, retrieve the animals that have three or more colors using the json_array_length() function:
+*/
+SELECT
+  *
+FROM animals;
+
+SELECT
+  id,
+  data ->> 'animal' AS animal,
+  data -> 'colors' AS color
+FROM animals
+WHERE JSON_ARRAY_LENGTH(data, '$.colors') >= 3;
